ab2791cefdd814788cbc1a295dd99504b5cd05e2
Loren Lima Martins <loren.lima@take.net>
2024-06-25 14:25:48 -0300

    feat: add two sum solution

---
+++ LeetCode/TwoSum/Solution.cs
@@ -0,0 +1,31 @@
+﻿//1.Two Sum:
+
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+//You can return the answer in any order.
+
+namespace LeetCode.TwoSum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+                if (dict.TryGetValue(diff, out int value))
+                {
+                    return new int[] { value, i };
+                }
+                if (!dict.ContainsKey(nums[i]))
+                {
+                    dict.Add(nums[i], i);
+                }
+            }
+            return new int[] { -1, -1 };
+        }
+    }
+}
