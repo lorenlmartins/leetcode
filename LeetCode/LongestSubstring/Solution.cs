caf69453603fe46e007dbae63eedf55b91052b6e
Loren Lima Martins <loren.lima@take.net>
2024-06-26 14:40:32 -0300

    feat: add longest substring without repeating characters

---
+++ LeetCode/LongestSubstring/Solution.cs
@@ -0,0 +1,32 @@
+﻿//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+namespace LeetCode.LongestSubstring
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+
+            int maxLength = 0;
+            int left = 0;
+            var charSet = new HashSet<char>();
+
+            for (int right = 0; right < s.Length; right++)
+            {
+                while (charSet.Contains(s[right]))
+                {
+                    charSet.Remove(s[left]);
+                    left++;
+                }
+                charSet.Add(s[right]);
+                maxLength = Math.Max(maxLength, right - left + 1);
+            }
+
+            return maxLength;
+        }
+    }
+}
